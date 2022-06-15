15ab8d068bb30d17ed995f33f19180f3b355c26d
Kevin Roney <ktroney.pdx@gmail.com>
2022-06-15 15:40:44 -0700

    completed exercise 12

--- exercise_3/exercise-3.sql
+++ exercise_3/exercise-3.sql
@@ -5,4 +5,4 @@ SELECT title
 FROM film
 LEFT JOIN film_category ON film.film_id = film_category.film_id
 LEFT JOIN category ON film_category.category_id = category.category_id
-WHERE category.name = 'Action';
+WHERE name = 'Action';
